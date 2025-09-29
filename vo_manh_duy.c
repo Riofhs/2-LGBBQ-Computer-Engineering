e50c10559c85b5b67837daf6afa930758e65dc56
wd747-wd <pemp3408@gmail.com>
2025-09-29 11:31:24 +0700

    feat: add vo_manh_duy.c (personal info)

--- vo_manh_duy.c
+++ vo_manh_duy.c
@@ -2,7 +2,7 @@
 // Mục đích: chứa thông tin cá nhân (Võ Mạnh Duy, 25119064) - K25 trường Đại học Sư Phạm Kỹ Thuật Hồ Chí Minh
 #include <stdio.h>
 int main(void) {
-    printf("Họ tên: Võ Mạnh Duy (07/04/2007) \n");
+    printf("Họ tên: Võ Mạnh Duy (07/04/2007)\n");
     printf("MSSV: 25119064\n");
     printf("Hiện tại, tôi đang là một tân sinh viên khóa k25 của trường Đại học Sư Phạm Kỹ Thuật Hồ Chí Minh. Tôi rất thích chơi đá banh\n");
     return 0;
